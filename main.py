8aafb680362618ed0425dffcba89b2e03f60e126
PaperDragon-SH <2678885646@qq.com>
2023-08-16 11:02:40 +0800

    更新权重驱动的词云，补全python依赖文件，添加忽略ide配置

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import re
 import string
 import jieba
+import jieba.analyse
 from collections import defaultdict
 
 # wordcloud
@@ -9,37 +10,32 @@ from wordcloud import WordCloud
 from PIL import Image
 import numpy as np
 
-
-MARKDOWN_ROOT_PATH="./src/note-book"
+MARKDOWN_ROOT_PATH = "./src/note-book"
 
 stop_words = ['你', '我', '的', '了', '们']
-
 
 FONT = "./MiSans-Normal.ttf"
 OUTPUT_IMG_NAME = "./wordcloud.png"
 WORDCLOUD_IMAGE_MASK = "./coding.jpg"
 
 
-
-
-
-def word_frequency(directory, key_nums = 200) -> list:
+def word_frequency(directory, key_nums=200):
     """
-    ''' param: directory markdown文件根目录
-    ''' param: key_nums 生成排名前 xx 个的列表，默认200
-    ''' return: list[key_nums]
+    param: directory markdown文件根目录
+    param: key_nums 生成排名前 xx 个的列表，默认200
+    return: list[key_nums]
     """
-
     # 初始化一个空字典来存储名词的词频
     noun_freq = defaultdict(int)
 
     wordcloud_item = []
+    wordcloud_dict = {}
 
     # 遍历目录下的所有文件
     for dirpath, dirnames, filenames in os.walk(directory):
         for filename in filenames:
             # 检查文件是否为markdown文件
-            if filename.endswith('.md') or filename.endswith('.markdown'):
+            if filename.endswith('.md') or filename.endswith('.txt'):
                 file_path = os.path.join(dirpath, filename)
                 # print(file_path)
                 with open(file_path, 'r', encoding='utf-8') as f:
@@ -54,14 +50,12 @@ def word_frequency(directory, key_nums = 200) -> list:
                         word = re.sub(
                             r'^(-|#|\.|\*|:|0-9|_|a-z|A-Z|0|png|the|to|一个|\+)*$', '', word)
                         word = re.sub(r'^assets.*$', '', word)
-                        # word = re.sub(r'^terminal.*$', '', word)
                         word = re.sub(r'^[a-z].*$', '', word)
                         word = re.sub(r'^[A-Z][A-Z][A-Z].*$', '', word)
-                        word = re.sub(r'^[A-Z][a-z][a-z].*$', '', word)
+                        word = re.sub(r'^[A-Z][a-z][a-z]$', '', word)
                         word = re.sub(r'^[0-9].*$', '', word)
-                        if len(word) < 4 or word == string.punctuation:  # 排除单个字符的分词结果
-                            continue
-                        if word == stop_words:
+                        word = re.sub(r'.*[个次]', '', word)
+                        if len(word) < 3 or word == string.punctuation:  # 排除单个字符的分词结果
                             continue
                         else:
                             noun_freq[word] += 1
@@ -72,27 +66,35 @@ def word_frequency(directory, key_nums = 200) -> list:
 
     print("{0:<5}{1:<8}{2:<5}".format('序号', '词语', '频率'))
 
-    # 需要显示的范围  10即显示前10个，0到9
+    # # 需要显示的范围  10即显示前10个，0到9
     for i in range(key_nums):
         word, count = items[i]
         wordcloud_item.append(word)
-        print("{0:<5}{1:<8}{2:>5}".format(i+1, word, count))
-    return wordcloud_item
+        wordcloud_dict[word] = count
+        print("{0:<5}{1:<8}{2:>5}".format(i + 1, word, count))
+    # return wordcloud_item
+
+    return wordcloud_dict
 
 
+# word_frequency(MARKDOWN_ROOT_PATH)
 
-mask = np.array(Image.open(f"{WORDCLOUD_IMAGE_MASK}"))
 
-text_cut = '/'.join(word_frequency(MARKDOWN_ROOT_PATH))
-print(text_cut)
-wordcloud = WordCloud(background_color="white",
-                      width=1080,
-                      height=1920,
-                      max_words=200,
-                      max_font_size=80,
-                      mask=mask,
-                      contour_width=3,
-                      contour_color='steelblue',
-                      font_path=f'{FONT}'
-                      ).generate(text_cut)
-wordcloud.to_file(f'{OUTPUT_IMG_NAME}')
+def create_wordlouod(wordcloud_item: dict = {"Shanghai": 30, "Beijing": 40}):
+    mask = np.array(Image.open(f"{WORDCLOUD_IMAGE_MASK}"))
+
+    wordcloud = WordCloud(background_color="white",
+                          width=1366,
+                          height=768,
+                          max_words=400,
+                          max_font_size=80,
+                        #   mask=mask,
+                          contour_width=3,
+                          contour_color='steelblue',
+                          font_path=f'{FONT}',
+                          stopwords=stop_words
+                          ).generate_from_frequencies(wordcloud_item)
+    wordcloud.to_file(f'{OUTPUT_IMG_NAME}')
+
+
+create_wordlouod(wordcloud_item=word_frequency(MARKDOWN_ROOT_PATH, key_nums=400))
