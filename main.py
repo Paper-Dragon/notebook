72f5592f8f66ecc56213091da01b5286a9c2eb5a
PaperDragon <2678885646@qq.com>
2023-10-29 20:03:04 +0800

    重新生成云图

--- main.py
+++ main.py
@@ -10,13 +10,24 @@ from wordcloud import WordCloud
 from PIL import Image
 import numpy as np
 
-MARKDOWN_ROOT_PATH = "./src/note-book"
+MARKDOWN_ROOT_PATH = "./src/"
 
-stop_words = ['你', '我', '的', '了', '们']
+stop_words = ['你', '我', '的', '了', '们', 'True', 'False', 'Node']
 
 FONT = "./MiSans-Normal.ttf"
 OUTPUT_IMG_NAME = "./wordcloud.png"
 WORDCLOUD_IMAGE_MASK = "./coding.jpg"
+RE_POLICY = (r"^(-|#|\.|\*|:|0-9|_|a-z|A-Z|0|png|the|to|RegionOne|"
+             r"服务器|配置文件|一个|接下来|软件包|自定义|默认值|字符串|尽可能|是因为|表达式|相当于|客户端|初始化|解决方案|"
+             r"优先级|运算符|管理员|"
+             r"True|False|"
+             r"KiB|MiB|GiB|"
+             r"Shanghai|Server|Client|Local|Address|Image|Runner|Label|Please|Master|Ctrl|"
+             r"List|Array|String|Field|Column|Name|Version|Live|Depends|Path|"
+             r"Creating|Running|Stopped|Applying|Installing|Pulling|Active|"
+             r"Enable|Optional|Ready|Files|Configure|Configuration|"
+             r"Pull|Node|None|"
+             r"User|Users|Default|Value|This|Type|File|Asia|\+)*$")
 
 
 def word_frequency(directory, key_nums=200):
@@ -47,8 +58,7 @@ def word_frequency(directory, key_nums=200):
 
                     # 统计名词的词频
                     for word in words:
-                        word = re.sub(
-                            r'^(-|#|\.|\*|:|0-9|_|a-z|A-Z|0|png|the|to|一个|\+)*$', '', word)
+                        word = re.sub(RE_POLICY, '', word)
                         word = re.sub(r'^assets.*$', '', word)
                         word = re.sub(r'^[a-z].*$', '', word)
                         word = re.sub(r'^[A-Z][A-Z][A-Z].*$', '', word)
@@ -88,7 +98,7 @@ def create_wordlouod(wordcloud_item: dict = {"Shanghai": 30, "Beijing": 40}):
                           height=768,
                           max_words=400,
                           max_font_size=80,
-                        #   mask=mask,
+                          #   mask=mask,
                           contour_width=3,
                           contour_color='steelblue',
                           font_path=f'{FONT}',
