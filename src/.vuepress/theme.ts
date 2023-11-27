60d677cd7d0d4b587e24f6e8586cfb1c899e6d32
PaperDragon-SH <2678885646@qq.com>
2023-11-27 13:19:31 +0800

    ci add artifacts build/upload for cloudflare

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,7 +3,7 @@ import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-  hostname: "paper-dragon.github.io",
+  hostname: "https://paper-dragon.github.io",
   
   fullscreen: true,
   
