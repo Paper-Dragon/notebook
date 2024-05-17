924a314839150ac51ac86bd1beb7cbc95d460648
PaperDragon-SH <2678885646@qq.com>
2024-05-17 13:28:19 +0800

    nvidia docker mirror

--- src/.vuepress/public/sh/nvidia/nvidia-driver-deploy.sh
+++ src/.vuepress/public/sh/nvidia/nvidia-driver-deploy.sh
@@ -247,8 +247,9 @@ if [[ ! -z "$NVIDIA_PRESENT" ]]; then
     else
         echo "nvidia-docker does not seem to be enabled. Proceeding with installations..."
         distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-        curl -s -L https://nvidia.geekery.cn/nvidia-docker/gpgkey | sudo apt-key add
-        curl -s -L https://nvidia.geekery.cn/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+        # nvidia.github.io <- nvidia-docker.geekery.cn
+        curl -s -L https://nvidia-docker.geekery.cn/nvidia-docker/gpgkey | sudo apt-key add
+        curl -s -L https://nvidia-docker.geekery.cn/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
         sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
         sudo systemctl restart docker 
         sudo docker run --gpus all nvidia/cuda:11.0.3-base-ubuntu18.04 nvidia-smi
